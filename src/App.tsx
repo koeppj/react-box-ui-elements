34a683dd30cb1f08d2c94fb7a9183e39f80651ff
John Koepp <jkoepp@box.com>
2025-03-21 20:17:05 -0400

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from "react-router-dom";
+import { AppBar, Toolbar, Typography, Box, CssBaseline } from '@mui/material';
+import { Dashboard, Folder, Group, Assessment, AccountCircle } from '@mui/icons-material';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Box sx={{ display: 'flex' }}>
+        <CssBaseline />
+        <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+          <Toolbar>
+            <img src={logo} className="App-logo" alt="logo" />
+            <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
+              My Application
+            </Typography>
+            <AccountCircle />
+          </Toolbar>
+        </AppBar>
+        </Box>
+    </Router>
   );
 }
 
