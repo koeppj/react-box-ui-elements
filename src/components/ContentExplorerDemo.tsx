de0865d2b7320a033983522a45a5fa427e0800ea
John Koepp <jkoepp@box.com>
2025-04-07 16:51:43 -0400

    EOD Update

--- src/components/ContentExplorerDemo.tsx
+++ src/components/ContentExplorerDemo.tsx
@@ -1,23 +1,25 @@
 import React, { useState } from 'react';
-import { Card, CardContent, Typography } from "@mui/material";
+import { Accordion,  Card, CardContent, Typography } from "@mui/material";
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
 import { useAuth } from "../contexts/AuthContext";
 import { useEffect } from "react";
 import ContentExplorer from "box-ui-elements/es/elements/content-explorer";
-import ContentPickerPopup from "box-ui-elements/es/elements/content-picker";
+import ContentPicker from "box-ui-elements/es/elements/content-picker";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 export function ContentExplorerDemo() {
 
     const {isAuthenticated, expriresIn, client, accessToken} = useAuth();
     const [token, setToken] = useState<string|undefined>(undefined);
     const [rootFolderId, setRootFolderId] = useState<string | undefined>("0");
+    const [isExpanded, setIsExpanded] = useState<string| false>(false);
 
     useEffect(() => {
         const init = async () => {
             if (isAuthenticated) {
                 // Get the access token for the component
                 accessToken().then((token) => {
-                    // Set the token in the state;
-                    console.debug("token", token);
                     setToken(token)
                 })
             }
@@ -28,17 +30,30 @@ export function ContentExplorerDemo() {
         init();
     },[isAuthenticated])
 
+    function handleAccordianChange(panelId: string) {
+        return (event: React.SyntheticEvent, isExpanded: boolean) => {
+            setIsExpanded(isExpanded ? panelId : false);
+        }
+    };
+
     return (
         <Card sx={{ my: 2, padding: 2,flexGrow: 1, display: 'flex', flexDirection: 'column', height: '80vh'}} id="content-explorer-demo">
           <Typography variant="h5" component="h2" gutterBottom>App Status</Typography>
             {token && (
-                <ContentPickerPopup 
-                    token={token}
-                    type="folder"
-                    canUpload={false}
-                    modal={{
-                        buttonLabel: "Select Folder",
-                    }} />
+                <div>
+                    <Accordion expanded={isExpanded === "folder-picker"} 
+                               onChange={handleAccordianChange("folder-picker")} >
+                        <AccordionSummary expandIcon={<ExpandMoreIcon />} >
+                            <Typography>Folder Picker</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <ContentPicker 
+                                token={token}
+                                type="folder"
+                                canUpload={false} />
+                        </AccordionDetails>
+                    </Accordion>
+                </div>
             )}        
             {token && (
                 <ContentExplorer sx= {{ lexGrow: 1, display: 'flex', flexDirection: 'column',}}
