19c4170c7374db3d3a3b2abfcfba35d5b386430f
John Koepp <johnkoepp@gmail.com>
2025-04-07 09:02:07 -0400

    Weekend Update

--- src/components/ContentExplorerDemo.tsx
+++ src/components/ContentExplorerDemo.tsx
@@ -1,22 +1,25 @@
 import React, { useState } from 'react';
-import { Typography } from "@mui/material";
+import { Card, CardContent, Typography } from "@mui/material";
 import { useAuth } from "../contexts/AuthContext";
 import { useEffect } from "react";
 import ContentExplorer from "box-ui-elements/es/elements/content-explorer";
-
+import ContentPickerPopup from "box-ui-elements/es/elements/content-picker";
 
 export function ContentExplorerDemo() {
 
     const {isAuthenticated, expriresIn, client, accessToken} = useAuth();
     const [token, setToken] = useState<string|undefined>(undefined);
+    const [rootFolderId, setRootFolderId] = useState<string | undefined>("0");
 
     useEffect(() => {
         const init = async () => {
             if (isAuthenticated) {
                 // Get the access token for the component
-                const token = await accessToken();
-                console.debug("token", token);
-                setToken(token)
+                accessToken().then((token) => {
+                    // Set the token in the state;
+                    console.debug("token", token);
+                    setToken(token)
+                })
             }
             else {
                 setToken(undefined);
@@ -26,14 +29,23 @@ export function ContentExplorerDemo() {
     },[isAuthenticated])
 
     return (
-        <div>
-            <Typography variant="h2">Welcome to the Box Content Explorer UI Widget</Typography>
+        <Card sx={{ my: 2, padding: 2,flexGrow: 1, display: 'flex', flexDirection: 'column', height: '80vh'}} id="content-explorer-demo">
+          <Typography variant="h5" component="h2" gutterBottom>App Status</Typography>
             {token && (
-                <ContentExplorer
+                <ContentPickerPopup 
                     token={token}
-                    rootFolderId="0"
+                    type="folder"
+                    canUpload={false}
+                    modal={{
+                        buttonLabel: "Select Folder",
+                    }} />
+            )}        
+            {token && (
+                <ContentExplorer sx= {{ lexGrow: 1, display: 'flex', flexDirection: 'column',}}
+                    token={token}
+                    rootFolderId={rootFolderId}
                 />
             )}
-        </div>
+        </Card>
     )
 }
