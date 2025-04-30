185405afa584ae9c342aeb690aa1271432a47b04
John Koepp <jkoepp@box.com>
2025-04-30 17:17:16 -0400

    EOD Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { AuthProvider } from './contexts/AuthContext';
 import { SnackbarProvider } from 'notistack';
-import logo from './logo.svg';
 import './App.css';
 import AppMain from './components/AppMain';
 import { ConfigProvider } from './contexts/ConfigContext';
 import { IntlProvider } from 'react-intl';
+import { messages } from "box-ui-elements/i18n/bundles/en-US";
 
 function App() {
 
@@ -13,7 +13,7 @@ function App() {
     <SnackbarProvider maxSnack={3}>
       <AuthProvider>
         <ConfigProvider>
-          <IntlProvider locale="en">
+          <IntlProvider locale="en" messages={messages}>
             <AppMain />
           </IntlProvider>
         </ConfigProvider>
---
+++ types/box-ui-elements__i18n__bundles_en-US/box-i18n-modules.d.ts
@@ -0,0 +1,27 @@
+// types/box-i18n-modules.d.ts
+
+declare module '@box/box-ai-agent-selector/i18n/en-US' {
+    const messages: Record<string, string>;
+    export default messages;
+  }
+  
+  declare module '@box/box-ai-content-answers/i18n/en-US' {
+    const messages: Record<string, string>;
+    export default messages;
+  }
+  
+  declare module '@box/item-icon/i18n/en-US' {
+    const messages: Record<string, string>;
+    export default messages;
+  }
+  
+  declare module '@box/metadata-editor/i18n/en-US' {
+    const messages: Record<string, string>;
+    export default messages;
+  }
+  
+  declare module 'box-i18n-bundles-en-US' {
+    const messages: Record<string, string>;
+    export default messages;
+  }
+  
